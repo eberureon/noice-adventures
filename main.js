d5b93939ca4a4a3fbf8ec77d1ef44015403111be
Leon Ebel <ebel.leon@hotmail.com>
2018-06-27 21:14:18 +0200

    optimized size for MAC OS

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const {app, BrowserWindow} = require('electron');
 let win;
 
 function createWindow () {
-  win = new BrowserWindow({width: 991, height: 655});
-
-  // TODO: use this to remove the menubar and resize the BrowserWindow height to 655
+  win = new BrowserWindow({width: 974, height: 640}); // MAC OS optimal Size
+  // win = new BrowserWindow({width: 991, height: 655); // Windows optimal Size
+  
   // win.setMenu(null);
   win.loadFile('index.html');
 
