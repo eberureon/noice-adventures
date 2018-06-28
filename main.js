748565ec82bcbe043f300d953f19d459736ae370
Leon Ebel <ebel@jochen-schweizer.de>
2018-06-28 16:27:46 +0200

    remove menu bar

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ const {app, BrowserWindow} = require('electron');
 let win;
 
 function createWindow () {
-  win = new BrowserWindow({width: 974, height: 640}); // MAC OS optimal Size
-  // win = new BrowserWindow({width: 991, height: 655); // Windows optimal Size
+  // win = new BrowserWindow({width: 974, height: 640}); // MAC OS optimal Size
+  win = new BrowserWindow({width: 991, height: 655}); // Windows optimal Size
   
-  // win.setMenu(null);
+  win.setMenu(null);
   win.loadFile('index.html');
 
   // win.webContents.openDevTools();
